d48402bfffadec97f2167a4c19a77a846cd5dd47
Carlos Vargas <cavargas40@gmail.com>
2019-09-17 11:10:35 -0500

    Routes package and simple routes

--- api-rest/main.go
+++ api-rest/main.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"github.com/gorilla/mux"
 )
 
 func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello World from my web server with GO")
-	})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contact", Contact)
 
 	fmt.Println("The server is running in http://localhost:8080")
-
-	server := http.ListenAndServe(":8080", nil)
-
+	server := http.ListenAndServe(":8080", router)
 	log.Fatal(server)
+}
+
+func Index(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello World from my web server with GO")
+}
+
+func Contact(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "This is the Contact Page")
 }
